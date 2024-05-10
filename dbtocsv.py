05025437b7786ea1e338389e4660ed2b351f42a5
Bruce Wang <brucew.boyang@gmail.com>
2024-05-10 17:17:31 -0400

    Update dbtocsv.py

--- dbtocsv.py
+++ dbtocsv.py
@@ -1,13 +1,24 @@
 import pyodbc 
-SERVER = '(local)'
-DATABASE = 'AdventureWorks2022'
+SERVER = 'BrucePC\SQLEXPRESS'
+DATABASE = 'AdventureWorksLT2022'
 connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};Encrypt=no;Trusted_connection=yes;'
 conn = pyodbc.connect(connectionString) 
 SQL_QUERY = """
-SELECT * FROM Sales.Customer
+SELECT 
+TOP 5 c.CustomerID, 
+c.CompanyName, 
+COUNT(soh.SalesOrderID) AS OrderCount 
+FROM 
+SalesLT.Customer AS c 
+LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID 
+GROUP BY 
+c.CustomerID, 
+c.CompanyName 
+ORDER BY 
+OrderCount DESC;
 """
 cursor = conn.cursor()
 cursor.execute(SQL_QUERY)
 records = cursor.fetchall()
 for r in records:
-    print(r)
+    print(f"{r.CustomerID}\t{r.OrderCount}\t{r.CompanyName}")
