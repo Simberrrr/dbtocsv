93332148e74dc57189431b6ae4a669c4c4dc15e5
Bruce Wang <brucew.boyang@gmail.com>
2024-05-10 17:07:01 -0400

    Update dbtocsv.py
    changed database and was able to read the tables properly as well as print out the records

--- dbtocsv.py
+++ dbtocsv.py
@@ -1,14 +1,13 @@
 import pyodbc 
 SERVER = '(local)'
-DATABASE = 'master'
+DATABASE = 'AdventureWorks2022'
 connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};Encrypt=no;Trusted_connection=yes;'
 conn = pyodbc.connect(connectionString) 
 SQL_QUERY = """
-SELECT * FROM DimGeography
+SELECT * FROM Sales.Customer
 """
 cursor = conn.cursor()
 cursor.execute(SQL_QUERY)
 records = cursor.fetchall()
 for r in records:
-    print(f"hu")
-print("EJ")
+    print(r)
